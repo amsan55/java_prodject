73b1ce9838db730cd4b700390a835f51b04daa10
amsan <amsan.tovik@gmail.com>
2021-06-02 18:59:50 +0200

    Leap year

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,14 +1,31 @@
 package org.example;
 
+import java.time.Year;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
 
+        int year = 365;
+        int leapyear = 366;
+
+
+        System.out.println("What year is it?");
     }
+
+        class leapYear {
+            String year;
+            String leapyear;
+
+        }
+
+
+
+
 }
+
+
